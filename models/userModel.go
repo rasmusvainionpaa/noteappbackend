736fa694b7e00b4238a4f6df76a6c3f98dbe79a1
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-09-20 15:46:32 +0300

    Create userModel.go

---
+++ models/userModel.go
@@ -0,0 +1,21 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	// id
+	// created at
+	// updated at
+	// deleted at
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `gorm:"unique" json:"email"`
+	Password  string `json:"password"`
+	Role      string `json:"role"`
+}
+
+type Authentication struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
