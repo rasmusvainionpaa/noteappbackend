bec3c19dcb1feaad1ef5348c41bf27eccebb450c
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-09-20 15:46:39 +0300

    Create userRoutes.go

---
+++ routes/userRoutes.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	usercontrollers "noteappbackend/controllers/userControllers"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func RegisterUserRoutes(gin *gin.Engine, db *gorm.DB) {
+	handler := &usercontrollers.UserHandler{
+		DB: db,
+	}
+
+	routes := gin.Group("/user")
+	routes.GET("/", handler.GetUser)
+	routes.GET("/:id", handler.GetUsers)
+	routes.PUT("/:id", handler.UpdateUser)
+	routes.POST("/", handler.CreateUser)
+	routes.DELETE("/:id", handler.DeleteUser)
+}
