2329338d1867b0af62c65bee741eb19b091c1a81
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-09-23 17:43:19 +0300

    Update noteModel.go

--- models/noteModel.go
+++ models/noteModel.go
@@ -1,6 +1,10 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Note struct {
 	gorm.Model // creates a gorm model that adds id and creation time
@@ -8,8 +12,9 @@ type Note struct {
 	// created at
 	// updated at
 	// deleted at
-	UserID         float32 `json:"userId"`
-	Title          string  `json:"title"`
-	Content        string  `json:"content"`
-	InsideHumidity float32 `json:"InsideHumidity"`
+	UserID    float32   `json:"userId"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	Alert     time.Time `json:"InsideHumidity"`
+	Important bool      `json:"isImportant"`
 }
