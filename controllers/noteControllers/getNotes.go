4c2bc4e62dd2bfa66a05870c2eccd0bcce4fb393
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-09-23 22:50:13 +0300

    Update getNotes.go

--- controllers/noteControllers/getNotes.go
+++ controllers/noteControllers/getNotes.go
@@ -1,9 +1,29 @@
 package notecontrollers
 
 import (
+	"net/http"
+	"noteappbackend/database"
+	"noteappbackend/models"
+
 	"github.com/gin-gonic/gin"
 )
 
 func GetNotes(c *gin.Context) {
+	var notes []models.Note
 
+	user, userExist := c.Get("user")
+
+	if userExist == false {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "can't find user",
+		})
+		return
+	}
+
+	if result := database.DB.Find(&notes, "user_id = ?", user.(models.User).ID); result.Error != nil {
+		c.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	c.JSON(http.StatusOK, &notes)
 }
