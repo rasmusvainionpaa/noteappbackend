bbed540fe753734feb80f49c4978fae3c3ce7e60
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-09-23 17:43:33 +0300

    Update getNote.go

--- controllers/noteControllers/getNote.go
+++ controllers/noteControllers/getNote.go
@@ -1,7 +1,22 @@
 package notecontrollers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func GetNote(c *gin.Context) {
+	user, userExist := c.Get("user")
 
+	if userExist == false {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "can-t find user",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"user": user,
+	})
 }
