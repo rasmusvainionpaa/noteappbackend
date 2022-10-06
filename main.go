75a50b97b28189a86cabe1e05c5f6152c597abc3
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-10-06 22:37:52 +0300

    Update main.go

--- main.go
+++ main.go
@@ -23,6 +23,20 @@ func main() {
 		fmt.Printf("Error loadin env file")
 	}
 
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
+
 	//add routes
 	routes.RegisterUserRoutes(router)
 	routes.RegisterNoteRoutes(router)
