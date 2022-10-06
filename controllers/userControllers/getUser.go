9782e665f6b293d8cdb0cebb513ddc8bd1bd56a1
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-10-06 22:37:38 +0300

    Update getUser.go

--- controllers/userControllers/getUser.go
+++ controllers/userControllers/getUser.go
@@ -1,9 +1,26 @@
 package usercontrollers
 
 import (
+	"net/http"
+	"noteappbackend/models"
+
 	"github.com/gin-gonic/gin"
 )
 
 func GetUser(c *gin.Context) {
+	user, userExist := c.Get("user")
 
+	if userExist == false {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "can't find user",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"id":        user.(models.User).ID,
+		"firstName": user.(models.User).FirstName,
+		"lastName":  user.(models.User).LastName,
+		"email":     user.(models.User).Email,
+	})
 }
